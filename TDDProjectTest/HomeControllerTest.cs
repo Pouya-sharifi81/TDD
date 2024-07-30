f17720f0b0663dfa31602d456849b91ded5f8f7b
pouya sharifi <pouyasharifi99@gmail.com>
2024-07-30 15:05:50 +0330

    Add project files.

---
+++ TDDProjectTest/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDProjectTest
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+         public void IndexTest()
+        {
+            //Arrange
+            HomeController homeController = new HomeController();
+            //Act
+            var result = homeController.Index();
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
